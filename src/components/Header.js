c6203c3fd2bb19242d03030e05c280eaab600304
NNikonov <n.nikonov008@gmail.com>
2022-04-14 18:41:53 +0300

    Release v1

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = ( {handleToggleDarkMode} ) => {
+    return (
+        <div className="header">
+            <h1>My Notes</h1>
+            <button 
+                onClick={() => handleToggleDarkMode((previousDarkMode) => !previousDarkMode)} 
+                className="save">Black&White Mode
+            </button>
+        </div>
+    )
+}
+
+export default Header;
